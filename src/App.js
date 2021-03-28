a62329d27be30fafb60bae7ac4aa54366620bc29
codewithdpk <codewithdeepak@yahoo.com>
2021-03-28 17:57:24 +0530

    Designs updated.

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import "./App.css";
 const App = () => {
   return (
     <div style={{ background: "black" }} className="cont">
-      <div style={{ margin: "150px" }}>
-        <span
-          style={{
-            color: "white",
-            fontWeight: "900",
-            fontSize: "80px",
-            marginRight: "200px",
-          }}
-        >
-          Hi 👋🏻 , I am Deepak Suthar. I am a Full Stack Developer 👨🏻‍💻 .
+      <div className="cont-left-side">
+        <span className="hola-text">Hola! 👋🏻</span>
+        <span className="introduction-text">
+          I am Deepak Suthar & I am a Frontend Developer. 🚀
         </span>
+        <a
+          href="https://firebasestorage.googleapis.com/v0/b/goodsoulapp.appspot.com/o/Deepak-cv.pdf?alt=media&token=6e20cc28-b1b1-4f45-9f3c-768b7498ecc0"
+          target="_blank"
+          style={{ alignSelf: "start", width: "100%" }}
+        >
+          <button className="download-resume-button">Download Resume 📄</button>
+        </a>
+      </div>
+      <div className="cont-right-side">
+        <img src={"/my_face_memoji.png"} className="right-image" />
       </div>
     </div>
   );
